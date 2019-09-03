d7f5a177f538688e57e6c98b33e8e72efccd7581
Aaron Henry <31551338+ahx97@users.noreply.github.com>
2019-09-03 13:56:20 -0500

    Function

---
+++ FunctionsMiniChallenge.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+func check(value: Int )-> String? {
+   
+    if( value <= 0){
+        return nil
+    }
+    
+    return String(value)
+}
+
+let numberString1: String? = check(value: 10)
+let numberString2: String? = check(value: 0)
+let numberString3: String? = check(value: -2)
+
+
+
